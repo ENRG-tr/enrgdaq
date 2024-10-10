99f5222ba2dd8799e2da43f7883c7e8e51799e62
Furkan <furkan.bilgin@outlook.com>
2024-10-10 23:10:25 +0300

    feat: filter message types for DAQJobStore

--- src/daq/store/models.py
+++ src/daq/store/models.py
@@ -1,5 +1,27 @@
-from daq.models import DAQJob
+import time
+from dataclasses import dataclass
+
+from daq.models import DAQJob, DAQJobMessage
 
 
 class DAQJobStore(DAQJob):
-    pass
+    allowed_message_types: list[type["DAQJobMessageStore"]]
+
+    def start(self):
+        while True:
+            self.consume()
+            time.sleep(0.5)
+
+    def handle_message(self, message: DAQJobMessage) -> bool:
+        is_message_allowed = False
+        for allowed_message_type in self.allowed_message_types:
+            if isinstance(message, allowed_message_type):
+                is_message_allowed = True
+        if not is_message_allowed:
+            raise Exception(f"Invalid message type: {type(message)}")
+        return super().handle_message(message)
+
+
+@dataclass
+class DAQJobMessageStore(DAQJobMessage):
+    daq_job: DAQJob
