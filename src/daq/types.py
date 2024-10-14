46f05ad8a3b49413f9eec95dec472b27eb4d600e
Furkan <furkan.bilgin@outlook.com>
2024-10-14 23:20:17 +0300

    feat: Add DAQJobHandleAlerts to job type dictionary

--- src/daq/types.py
+++ src/daq/types.py
@@ -1,6 +1,7 @@
 from daq.alert.alert_slack import DAQJobAlertSlack
 from daq.base import DAQJob
 from daq.jobs.caen.n1081b import DAQJobN1081B
+from daq.jobs.handle_alerts import DAQJobHandleAlerts
 from daq.jobs.handle_stats import DAQJobHandleStats
 from daq.jobs.healthcheck import DAQJobHealthcheck
 from daq.jobs.serve_http import DAQJobServeHTTP
@@ -15,6 +16,7 @@ DAQ_JOB_TYPE_TO_CLASS: dict[str, type[DAQJob]] = {
     "store_root": DAQJobStoreROOT,
     "serve_http": DAQJobServeHTTP,
     "handle_stats": DAQJobHandleStats,
+    "handle_alerts": DAQJobHandleAlerts,
     "alert_slack": DAQJobAlertSlack,
     "healthcheck": DAQJobHealthcheck,
 }
