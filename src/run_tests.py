46cd081e280fa8d4471f6638416b002b86b3092c
Furkan Bilgin <info@furkanbilgin.net>
2025-01-09 14:56:14 +0300

    feat: add coverage.py in run_tests.py

--- src/run_tests.py
+++ src/run_tests.py
@@ -1,11 +1,18 @@
 import unittest
+import os
+import coverage
 
 if __name__ == "__main__":
     loader = unittest.TestLoader()
-    import os
-
+    cov = coverage.Coverage(omit=["*/config-3.py", "*/config.py"])
+    cov.start()
     suite = loader.discover(
         os.path.join(os.path.dirname(__file__), "tests"), pattern="test_*.py"
     )
     runner = unittest.TextTestRunner()
     runner.run(suite)
+
+    cov.stop()
+    cov.save()
+
+    cov.report()
